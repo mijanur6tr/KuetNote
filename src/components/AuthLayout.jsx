46b90def82754e997b797e307ff2ce9749bb0797
Mijanur <mijanur6tr123@gmail.com>
2025-06-27 08:28:47 +0600

    components totally completed.

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React ,{ useState,useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 
-export const AuthLayout = (props) => {
-  return(
-    <div>auth</div>
-   )
+const AuthLayout = ({children,authentication=true}) => {
+
+  const navigate = useNavigate();
+  const [loader,setLoader] = useState(true);
+  const authStatus = useSelector((state)=>state.auth.status)
+
+  useEffect(()=>{
+
+    if(authentication && authStatus !== authentication){
+        navigate("/login")
+    }else if(!authentication && authStatus !== authentication){
+        navigate("/")
+    }
+
+    setLoader(false)
+
+  },[authStatus,navigate,authentication])
+
+  return loader ? <p>Loading...</p>:<>{children}</>;
   }
+
+  export default AuthLayout;
